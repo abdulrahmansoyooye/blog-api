cb1bfc645e84bac04c9330cc641d8c2b2e02c181
abdulrahmansoyooye <abdulrahmansoyooye@gmail.com>
2023-08-17 18:31:12 +0100

    use cors again

--- index.js
+++ index.js
@@ -30,9 +30,10 @@ const app = express();
 app.use(express.json());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
   next();
 });
 app.use(cors({
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -24,6 +24,7 @@ const upload = multer({ storage });
 router.post("/", upload.single("image"), async (req, res) => {
 
 
+ 
 
 
   const token = await req.headers.authorization;
