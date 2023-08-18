d7f494f6bd85f9685ca2ece6309681a2019866df
abdulrahmansoyooye <abdulrahmansoyooye@gmail.com>
2023-08-18 15:49:20 +0100

    use cors again

--- index.js
+++ index.js
@@ -29,10 +29,13 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use("/uploads", express.static(__dirname + "/uploads"));
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Custom-Header');
-  next();
-});
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 app.use(cors({
   origin:  '*', // Specify the allowed origin(s)
   credentials: true, // Allow credentials (cookies, headers)
